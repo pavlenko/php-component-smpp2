956c166a50540620472388076f764cc9ddea3f74
Pavlenko Eugene <pavlenko.obs@gmail.com>
2022-07-04 15:01:12 +0300

    add logic

--- src/PDU/PDU.php
+++ src/PDU/PDU.php
@@ -6,6 +6,28 @@ use PE\SMPP\Builder;
 
 abstract class PDU
 {
+    public const GENERIC_NACK          = 0x80000000;
+    public const BIND_RECEIVER         = 0x00000001;
+    public const BIND_RECEIVER_RESP    = 0x80000001;
+    public const BIND_TRANSMITTER      = 0x00000002;
+    public const BIND_TRANSMITTER_RESP = 0x80000002;
+    public const QUERY_SM              = 0x00000003;
+    public const QUERY_SM_RESP         = 0x80000003;
+    public const SUBMIT_SM             = 0x00000004;
+    public const SUBMIT_SM_RESP        = 0x80000004;
+    public const DELIVER_SM            = 0x00000005;
+    public const DELIVER_SM_RESP       = 0x80000005;
+    public const UNBIND                = 0x00000006;
+    public const UNBIND_RESP           = 0x80000006;
+    public const REPLACE_SM            = 0x00000007;
+    public const REPLACE_SM_RESP       = 0x80000007;
+    public const CANCEL_SM             = 0x00000008;
+    public const CANCEL_SM_RESP        = 0x80000008;
+    public const BIND_TRANSCEIVER      = 0x00000009;
+    public const BIND_TRANSCEIVER_RESP = 0x80000009;
+    public const ENQUIRE_LINK          = 0x00000015;
+    public const ENQUIRE_LINK_RESP     = 0x80000015;
+
     private int $commandStatus = 0;
     private int $sequenceNumber = 1;
     private string $body;
