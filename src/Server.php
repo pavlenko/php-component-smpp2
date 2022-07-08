f8ddbed1765d9e57f358ae81326f4a35bedc57e7
Pavlenko Eugene <pavlenko.obs@gmail.com>
2022-07-08 09:04:30 +0300

    add logic

--- src/Server.php
+++ src/Server.php
@@ -60,44 +60,47 @@ final class Server
             $this->clients[] = $this->master->accept();
         }
 
-        // Handle read data
-        //TODO split loop to foreach($r as $client){} && foreach($clients as $client){ if(in_array($client, $r)){continue} }
-        foreach ($this->clients as $key => $client) {
-            if (in_array($client, $r)) {
-                $head = $client->readData(16);
+        // Handle incoming data
+        foreach ($r as $client) {
+            $head = $client->readData(16);
 
-                if ('' === $head) {
-                    $client->close();
-                    unset($this->clients[$key]);
-                    continue;
-                }
-
-                $buffer = new Buffer($head);
-                if ($buffer->bytesLeft() < 16) {
-                    throw new \RuntimeException('Malformed PDU header');
-                }
-
-                $length        = $buffer->shiftInt32();
-                $commandID     = $buffer->shiftInt32();
-                $commandStatus = $buffer->shiftInt32();
-                $sequenceNum   = $buffer->shiftInt32();
-
-                $body = (string) $client->readData($length);
-                if (strlen($body) < $length - 16) {
-                    throw new \RuntimeException('Malformed PDU body');
-                }
-
-                new Command($commandID, $commandStatus, $sequenceNum, $body);
-            } else {
-                //TODO create enquire link pdu
-                //TODO maybe add to session: __construct(Stream), readPDU(): PDU, sendPDU(PDU): void
-                $num = $client->sendData('ENQUIRE_LINK');
-                if (0 === $num) {
-                    $client->close();
-                    unset($this->clients[$key]);
-                }
+            if ('' === $head) {
+                $client->close();
+                unset($this->clients[array_search($client, $this->clients)]);
+                continue;
             }
+
+            $buffer = new Buffer($head);
+            if ($buffer->bytesLeft() < 16) {
+                throw new \RuntimeException('Malformed PDU header');
+            }
+
+            $length        = $buffer->shiftInt32();
+            $commandID     = $buffer->shiftInt32();
+            $commandStatus = $buffer->shiftInt32();
+            $sequenceNum   = $buffer->shiftInt32();
+
+            $body = (string) $client->readData($length);
+            if (strlen($body) < $length - 16) {
+                throw new \RuntimeException('Malformed PDU body');
+            }
+
+            new Command($commandID, $commandStatus, $sequenceNum, $body);
         }
 
+        // Handle rest clients connected check
+        foreach ($this->clients as $key => $client) {
+            if (!in_array($client, $r)) {
+                continue;
+            }
+
+            //TODO create enquire link pdu
+            //TODO maybe add to session: __construct(Stream), readPDU(): PDU, sendPDU(PDU): void
+            $num = $client->sendData('ENQUIRE_LINK');
+            if (0 === $num) {
+                $client->close();
+                unset($this->clients[$key]);
+            }
+        }
     }
 }
