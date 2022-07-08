257222a1522742655018d9a222de3d13decd8038
Pavlenko Eugene <pavlenko.obs@gmail.com>
2022-07-08 09:01:58 +0300

    add logic

--- src/Server.php
+++ src/Server.php
@@ -44,65 +44,60 @@ final class Server
         }
     }
 
-    //TODO rename to tick & remove infinite loop
-    public function run(): void
+    public function tick(): void
     {
-        $master  = Stream::createServer('127.0.0.1');
-        $clients = [];
+        $r = array_merge([$this->master], $this->clients);
+        $n = null;
 
-        while (true) {
-            $r = array_merge([$master], $clients);
-            $n = null;
+        // Check streams first
+        if (0 === Stream::select($r, $n, $n)) {
+            return;
+        }
 
-            // Check streams first
-            if (0 === Stream::select($r, $n, $n)) {
-                continue;
-            }
+        // Handle new connections
+        if (in_array($this->master, $r)) {
+            unset($r[array_search($this->master, $r)]);
+            $this->clients[] = $this->master->accept();
+        }
 
-            // Handle new connections
-            if (in_array($master, $r)) {
-                unset($r[array_search($master, $r)]);
-                $clients[] = $master->accept();
-            }
+        // Handle read data
+        //TODO split loop to foreach($r as $client){} && foreach($clients as $client){ if(in_array($client, $r)){continue} }
+        foreach ($this->clients as $key => $client) {
+            if (in_array($client, $r)) {
+                $head = $client->readData(16);
 
-            // Handle read data
-            //TODO split loop to foreach($r as $client){} && foreach($clients as $client){ if(in_array($client, $r)){continue} }
-            foreach ($clients as $key => $client) {
-                if (in_array($client, $r)) {
-                    $head = $client->readData(16);
+                if ('' === $head) {
+                    $client->close();
+                    unset($this->clients[$key]);
+                    continue;
+                }
 
-                    if ('' === $head) {
-                        $client->close();
-                        unset($clients[$key]);
-                        continue;
-                    }
+                $buffer = new Buffer($head);
+                if ($buffer->bytesLeft() < 16) {
+                    throw new \RuntimeException('Malformed PDU header');
+                }
 
-                    $buffer = new Buffer($head);
-                    if ($buffer->bytesLeft() < 16) {
-                        throw new \RuntimeException('Malformed PDU header');
-                    }
+                $length        = $buffer->shiftInt32();
+                $commandID     = $buffer->shiftInt32();
+                $commandStatus = $buffer->shiftInt32();
+                $sequenceNum   = $buffer->shiftInt32();
 
-                    $length        = $buffer->shiftInt32();
-                    $commandID     = $buffer->shiftInt32();
-                    $commandStatus = $buffer->shiftInt32();
-                    $sequenceNum   = $buffer->shiftInt32();
+                $body = (string) $client->readData($length);
+                if (strlen($body) < $length - 16) {
+                    throw new \RuntimeException('Malformed PDU body');
+                }
 
-                    $body = (string) $client->readData($length);
-                    if (strlen($body) < $length - 16) {
-                        throw new \RuntimeException('Malformed PDU body');
-                    }
-
-                    new Command($commandID, $commandStatus, $sequenceNum, $body);
-                } else {
-                    //TODO create enquire link pdu
-                    //TODO maybe add to session: __construct(Stream), readPDU(): PDU, sendPDU(PDU): void
-                    $num = $client->sendData('ENQUIRE_LINK');
-                    if (0 === $num) {
-                        $client->close();
-                        unset($clients[$key]);
-                    }
+                new Command($commandID, $commandStatus, $sequenceNum, $body);
+            } else {
+                //TODO create enquire link pdu
+                //TODO maybe add to session: __construct(Stream), readPDU(): PDU, sendPDU(PDU): void
+                $num = $client->sendData('ENQUIRE_LINK');
+                if (0 === $num) {
+                    $client->close();
+                    unset($this->clients[$key]);
                 }
             }
         }
+
     }
 }
