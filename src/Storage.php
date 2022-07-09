5eca83be7c18480c980186cdb36e65cded42389b
Pavlenko Eugene <pavlenko.obs@gmail.com>
2022-07-09 08:35:36 +0300

    add logic

---
+++ src/Storage.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace PE\SMPP;
+
+//TODO here we can permanently store packets for future send
+//TODO this will allow no dependency on active server or client
+class Storage
+{
+    //TODO add, get, del, all, encode/decode for store data
+}
