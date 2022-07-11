ac35cb4243c747fc72ac9719e47eb5a127418ab6
Pavlenko Eugene <pavlenko.obs@gmail.com>
2022-07-11 15:50:59 +0300

    add logic

--- demo/client.php
+++ demo/client.php
@@ -2,42 +2,27 @@
 
 namespace PE\SMPP;
 
-use PE\SMPP\PDU\Address;
+use PE\Component\Loop\Loop;
 use PE\SMPP\Util\Stream;
 use Psr\Log\LogLevel;
-use React\EventLoop\Loop;
-use React\EventLoop\StreamSelectLoop;
 use Symfony\Component\Console\Logger\ConsoleLogger;
 use Symfony\Component\Console\Output\ConsoleOutput;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $logger = new ConsoleLogger(new ConsoleOutput(ConsoleOutput::VERBOSITY_DEBUG));
-
-$stream = Stream::createClient('127.0.0.1:2775', null, Session::TIMEOUT_CONNECT)->getResource();
-
-$loop = new StreamSelectLoop();
-$loop->addPeriodicTimer(0.5, fn() => $logger->log(LogLevel::DEBUG, 'TICK'));
-$loop->addReadStream($stream, function () {
-    var_dump(func_get_args());
-});
-$loop->addWriteStream($stream, function () {
-    var_dump(func_get_args());
-});
-$loop->run();
-return;
-//$client = new Client2('127.0.0.1:2775', 'user', 'pass', $logger);
-//$client->init();
-//$client->send(
-//    new Address(Address::TON_INTERNATIONAL, Address::NPI_ISDN, '1234567890'),
-//    new Address(Address::TON_INTERNATIONAL, Address::NPI_ISDN, '1234567890'),
-//    'TEST'
-//);
+$client = Stream::createClient('127.0.0.1:2775', null, Session::TIMEOUT_CONNECT);
+$client->setBlocking(false);
 
 $loop = new Loop(10);
-$loop->addPeriodicTimer(0.5, fn() => $client->tick());
-$loop->addSingularTimer(60, function (Loop $loop) use ($client) {
-    $loop->stop();
-    //$client->stop();
+$loop->addSingularTimer(30, fn(Loop $loop) => $loop->stop());
+$loop->addPeriodicTimer(1, function () use ($client, $logger) {
+    $r = [$client];
+    $n = [];
+    Stream::select($r, $n, $n, 0.01);
+
+    if (!empty($r)) {
+        $logger->log(LogLevel::DEBUG, 'READ: ' . $client->readData(1024));
+    }
 });
 $loop->run();
--- demo/server.php
+++ demo/server.php
@@ -3,15 +3,35 @@
 namespace PE\SMPP;
 
 use PE\Component\Loop\Loop;
+use PE\SMPP\Util\Stream;
+use Psr\Log\LogLevel;
 use Symfony\Component\Console\Logger\ConsoleLogger;
 use Symfony\Component\Console\Output\ConsoleOutput;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $logger = new ConsoleLogger(new ConsoleOutput(ConsoleOutput::VERBOSITY_DEBUG));
-$server = new ServerV2('127.0.0.1:2775', $logger);
+$server = Stream::createServer('127.0.0.1:2775');
+$client = null;
 
 $loop = new Loop(10);
 $loop->addSingularTimer(30, fn(Loop $loop) => $loop->stop());
+$loop->addPeriodicTimer(0.01, function () use ($server, &$client, $logger) {
+    $r = [$server];
+    $n = [];
+    Stream::select($r, $n, $n, 0.05);
 
-$server->run($loop);
+    if (!empty($r)) {
+        $logger->log(LogLevel::DEBUG, 'ACCEPT');
+        $client = $server->accept();
+        $client->sendData('TEST');
+    }
+});
+$loop->addPeriodicTimer(5, function () use ($server, &$client, $logger) {
+    if (!$client) {
+        return;
+    }
+    $logger->log(LogLevel::DEBUG, 'SEND');
+    $client->sendData('TEST');
+});
+$loop->run();
