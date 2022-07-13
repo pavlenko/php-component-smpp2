831d6badabb0507898af2de82378886d0d63ab7e
Pavlenko Eugene <pavlenko.obs@gmail.com>
2022-07-13 08:10:19 +0300

    add logic

--- demo/client.php
+++ demo/client.php
@@ -3,32 +3,20 @@
 namespace PE\SMPP;
 
 use PE\Component\Loop\Loop;
-use PE\SMPP\Util\Stream;
-use Psr\Log\LogLevel;
-use Symfony\Component\Console\Logger\ConsoleLogger;
-use Symfony\Component\Console\Output\ConsoleOutput;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$logger = new ConsoleLogger(new ConsoleOutput(ConsoleOutput::VERBOSITY_DEBUG));
-$client = Stream::createClient('127.0.0.1:2775', null, Session::TIMEOUT_CONNECT);
-$client->setBlocking(false);
+$logger = new LoggerSTDOUT(LoggerSTDOUT::VERBOSITY_DEBUG);
+$client = new Client('127.0.0.1:2775', 'SYSTEM_ID', null, $logger);
+$client->init();
 
 $loop = new Loop(10);
 $loop->addSingularTimer(30, fn(Loop $loop) => $loop->stop());
-$loop->addPeriodicTimer(0.05, function (Loop $loop) use ($client, $logger) {
-    $r = [$client];
-    $n = [];
-    Stream::select($r, $n, $n, 0.01);
-
-    if (!empty($r)) {
-        $logger->log(LogLevel::DEBUG, 'READ: ' . $client->readData(1024));
-        if ($client->isEOF()) {
-            $loop->stop();
-            return;
-        }
-        $client->sendData('TEST');
-        //stream_socket_sendto($client->getResource(), 'TEST');
+$loop->addPeriodicTimer(0.1, function (Loop $loop) use ($client) {
+    if (!$client->tick()) {
+        $loop->stop();
     }
 });
 $loop->run();
+
+$client->stop();
--- demo/server.php
+++ demo/server.php
@@ -3,42 +3,16 @@
 namespace PE\SMPP;
 
 use PE\Component\Loop\Loop;
-use PE\SMPP\Util\Stream;
-use Psr\Log\LogLevel;
-use Symfony\Component\Console\Logger\ConsoleLogger;
-use Symfony\Component\Console\Output\ConsoleOutput;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$logger = new ConsoleLogger(new ConsoleOutput(ConsoleOutput::VERBOSITY_DEBUG));
-$server = Stream::createServer('127.0.0.1:2775');
-$client = null;
+$logger = new LoggerSTDOUT(LoggerSTDOUT::VERBOSITY_DEBUG);
+$server = new Server('127.0.0.1:2775', $logger);
+$server->init();
 
 $loop = new Loop(10);
 $loop->addSingularTimer(30, fn(Loop $loop) => $loop->stop());
-$loop->addPeriodicTimer(0.01, function () use ($server, &$client, $logger) {
-    $r = [$server];
-    $n = [];
-    Stream::select($r, $n, $n, 0.05);
-
-    if (!empty($r)) {
-        $logger->log(LogLevel::DEBUG, 'ACCEPT');
-        $client = $server->accept();
-    }
-});
-$t = $loop->addPeriodicTimer(5, function (Loop $loop) use ($server, &$client, $logger, &$t) {
-    if (!$client) {
-        return;
-    }
-    $logger->log(LogLevel::DEBUG, 'SEND');
-
-    $num = $client->sendData('TEST');
-    $res = $client->readData(1024);
-    if ($num === 0 || '' === $res) {
-        $logger->log(LogLevel::DEBUG, 'CLOSE CLIENT');
-        $loop->removeTimer($t);
-        return;
-    }
-    $logger->log(LogLevel::DEBUG, 'RESP: ' . $res);
-});
+$loop->addPeriodicTimer(0.1, fn(Loop $loop) => $server->tick());
 $loop->run();
+
+$server->stop();
