557a50ae3f5d1a8a4dc642ee34aab0450d60c4eb
Pavlenko Eugene <pavlenko.obs@gmail.com>
2022-07-14 22:25:56 +0300

    add logic

--- demo/sender.php
+++ demo/sender.php
@@ -9,8 +9,8 @@ $address  = 'tcp://127.0.0.1:2775' || 'tls://127.0.0.1:8775';
 $systemID = 'SENDER_SYSTEM_ID';
 $password = 'PASSWORD';
 
-$logger  = new NullLogger();
-$storage = [];// InMemory|Redis|MySQL(?)
+$logger  = null;
+$storage = null;
 
 // V1
 $sender = new Sender($address, $systemID, $password, $logger, $storage);
@@ -18,8 +18,14 @@ $sender->send('1234', '1234', 'MESSAGE');
 
 // V2
 $sender = new Sender($address, $systemID, $password, $logger, $storage);
-$sender->bind($type);// Transmitter, etc
 $sender->send('1234', '1234', 'MESSAGE');
+
+interface ConnectionInterface {
+    public function open();//TODO <-- server/client/sender specific
+    public function bind();//TODO <-- server/client/sender specific
+    public function readPDU();
+    public function sendPDU($pduData);
+}
 
 interface StorageInterface {}
 
@@ -33,6 +39,8 @@ class Sender
 
     private LoggerInterface $logger;
     private StorageInterface $storage;
+
+    private ConnectionInterface $connection;
 
     public function __construct(
         string $address,
@@ -48,8 +56,21 @@ class Sender
         $this->storage  = $storage ?: new StorageNull();
     }
 
-    public function send($from, $to, string $message): array
+    public function send($from, $to, string $message): string
     {
-        return [];
+        if ($this->connection->getStatus() == 'init') {
+            $this->connection->open();// status -> open
+            $this->connection->sendPDU('BIND');
+            $this->connection->readPDU('BIND_RESP');// status -> bound
+        }
+        $this->connection->sendPDU('SUBMIT_SM');
+        return $this->connection->readPDU('SUBMIT_SM_RESP')['message_id'];
+    }
+
+    public function exit()
+    {
+        $this->connection->sendPDU('UNBIND');
+        $this->connection->readPDU('UNBIND_RESP');
+        $this->connection->exit();//<-- may be reset here for allow re-connect
     }
 }
