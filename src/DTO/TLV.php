8cb037ae4154dd7a7a467c869855157e90d51516
Pavlenko Eugene <pavlenko.obs@gmail.com>
2023-03-02 13:15:28 +0200

    cleanup

--- src/DTO/TLV.php
+++ src/DTO/TLV.php
@@ -2,8 +2,53 @@
 
 namespace PE\Component\SMPP\DTO;
 
-final class TLV implements TLVInterface
+final class TLV
 {
+    public const TAG_DEST_ADDR_SUBUNIT           = 0x0005;
+    public const TAG_DEST_NETWORK_TYPE           = 0x0006;
+    public const TAG_DEST_BEARER_TYPE            = 0x0007;
+    public const TAG_DEST_TELEMATICS_ID          = 0x0008;
+    public const TAG_SOURCE_ADDR_SUBUNIT         = 0x000D;
+    public const TAG_SOURCE_NETWORK_TYPE         = 0x000E;
+    public const TAG_SOURCE_BEARER_TYPE          = 0x000F;
+    public const TAG_SOURCE_TELEMATICS_ID        = 0x0010;
+    public const TAG_QOS_TIME_TO_LIVE            = 0x0017;
+    public const TAG_PAYLOAD_TYPE                = 0x0019;
+    public const TAG_ADDITIONAL_STATUS_INFO_TEXT = 0x001D;
+    public const TAG_RECEIPTED_MESSAGE_ID        = 0x001E;
+    public const TAG_MS_MSG_WAIT_FACILITIES      = 0x0030;
+    public const TAG_PRIVACY_INDICATOR           = 0x0201;
+    public const TAG_SOURCE_SUBADDRESS           = 0x0202;
+    public const TAG_DEST_SUBADDRESS             = 0x0203;
+    public const TAG_USER_MESSAGE_REFERENCE      = 0x0204;
+    public const TAG_USER_RESPONSE_CODE          = 0x0205;
+    public const TAG_SOURCE_PORT                 = 0x020A;
+    public const TAG_DESTINATION_PORT            = 0x020B;
+    public const TAG_SAR_MSG_REF_NUM             = 0x020C;
+    public const TAG_LANGUAGE_INDICATOR          = 0x020D;
+    public const TAG_SAR_TOTAL_SEGMENTS          = 0x020E;
+    public const TAG_SAR_SEGMENT_SEQNUM          = 0x020F;
+    public const TAG_SC_INTERFACE_VERSION        = 0x0210;
+    public const TAG_CALLBACK_NUM_PRES_IND       = 0x0302;
+    public const TAG_CALLBACK_NUM_ATAG           = 0x0303;
+    public const TAG_NUMBER_OF_MESSAGES          = 0x0304;
+    public const TAG_CALLBACK_NUM                = 0x0381;
+    public const TAG_DPF_RESULT                  = 0x0420;
+    public const TAG_SET_DPF                     = 0x0421;
+    public const TAG_MS_AVAILABILITY_STATUS      = 0x0422;
+    public const TAG_NETWORK_ERROR_CODE          = 0x0423;
+    public const TAG_MESSAGE_PAYLOAD             = 0x0424;
+    public const TAG_DELIVERY_FAILURE_REASON     = 0x0425;
+    public const TAG_MORE_MESSAGES_TO_SEND       = 0x0426;
+    public const TAG_MESSAGE_STATE               = 0x0427;
+    public const TAG_USSD_SERVICE_OP             = 0x0501;
+    public const TAG_DISPLAY_TIME                = 0x1201;
+    public const TAG_SMS_SIGNAL                  = 0x1203;
+    public const TAG_MS_VALIDITY                 = 0x1204;
+    public const TAG_ALERT_ON_MESSAGE_DELIVERY   = 0x130C;
+    public const TAG_ITS_REPLY_TYPE              = 0x1380;
+    public const TAG_ITS_SESSION_INFO            = 0x1383;
+
     private int $tag;
     private string $value;
 
