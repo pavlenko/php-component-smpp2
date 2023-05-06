2139e92e5535c514608b1fb3e14c6bddda9a1eae
Pavlenko Eugene <pavlenko.obs@gmail.com>
2023-05-06 08:55:10 +0300

    upd

---
+++ src/Socket/SocketInterface.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace PE\Component\SMPP\Socket;
+
+use PE\Component\Stream\Exception\RuntimeException;
+
+//TODO replacement for Stream(), because last if for other things
+//TODO void setCrypto()
+//TODO bool $encrypted
+interface SocketInterface
+{
+    /**
+     * Get resource pointer if available
+     *
+     * @return resource|null
+     * @internal DO NOT USE IN APP DIRECTLY
+     */
+    public function getResource();
+
+    /**
+     * Get full client/remote address of socket
+     *
+     * @param bool $remote
+     * @return string|null
+     */
+    public function getAddress(bool $remote = false): ?string;
+
+    /**
+     * Set read/write timeout
+     *
+     * @param int $seconds
+     * @param int $micros
+     */
+    public function setTimeout(int $seconds, int $micros = 0): void;
+
+    /**
+     * Set blocking/non-blocking mode
+     *
+     * @param bool $enable
+     */
+    public function setBlocking(bool $enable): void;
+
+    /**
+     * Set the read buffer
+     *
+     * @param int $size The number of bytes to buffer. If <b>$size</b> is 0 then operations are unbuffered
+     */
+    public function setBufferRD(int $size): void;
+
+    /**
+     * Set the write buffer
+     *
+     * @param int $size The number of bytes to buffer. If <b>$size</b> is 0 then operations are unbuffered
+     */
+    public function setBufferWR(int $size): void;
+
+    /**
+     * Check if stream closed by remote
+     *
+     * @return bool
+     */
+    public function isEOF(): bool;
+
+    /**
+     * Read data from stream
+     *
+     * @return string
+     * @throws RuntimeException
+     */
+    public function recv(): string;
+
+    /**
+     * Send data to stream
+     *
+     * @param string $data
+     * @return int
+     */
+    public function send(string $data): int;
+
+    /**
+     * Close stream
+     */
+    public function close(): void;
+}
