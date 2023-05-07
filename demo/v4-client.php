60db5e534ae7ec330bc953102a3b33cf95d85e01
Pavlenko Eugene <pavlenko.obs@gmail.com>
2023-05-07 08:03:13 +0300

    upd

---
+++ demo/v4-client.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace PE\Component\SMPP\V4;
+
+use PE\Component\Loop\Loop;
+use PE\Component\SMPP\Socket\Factory;
+use PE\Component\SMPP\Socket\Select;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$select  = new Select();
+$factory = new Factory($select);
+$loop    = new Loop(1, \Closure::fromCallable([$select, 'dispatch']));
+
+$client = $factory->createClient('127.0.0.1:2775');
+$client->setInputHandler(function (string $data) use ($client) {
+    echo "$data\n";
+    $client->write("HELLO\n");
+});
+$client->setErrorHandler(function (\Throwable $throwable) {
+    echo $throwable . "\n";
+});
+$client->setCloseHandler(function (string $message) use ($loop) {
+    echo $message . "\n";
+    $loop->stop();
+});
+
+$loop->run();
--- demo/v4-server.php
+++ demo/v4-server.php
@@ -1,10 +1,36 @@
 <?php
 
-namespace PE\Component\SMPP\V3;
+namespace PE\Component\SMPP\V4;
 
-use PE\Component\SMPP\Server4;
+use PE\Component\Loop\Loop;
+use PE\Component\SMPP\Socket\ClientInterface;
+use PE\Component\SMPP\Socket\Factory;
+use PE\Component\SMPP\Socket\Select;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$server = new Server4();
-$server->bind('127.0.0.1:2775');
+$select  = new Select();
+$factory = new Factory($select);
+$loop    = new Loop(1, \Closure::fromCallable([$select, 'dispatch']));
+
+$client = null;
+$server = $factory->createServer('127.0.0.1:2775');
+$server->setInputHandler(function (ClientInterface $socket) use (&$client) {
+    $client = $socket;
+    $client->setInputHandler(function (string $data) {
+        echo "$data\n";
+    });
+    $client->write("WELCOME\n");
+    echo "new connection from {$client->getClientAddress()}\n";
+});
+$server->setErrorHandler(function (\Throwable $throwable) {
+    echo $throwable . "\n";
+});
+$server->setCloseHandler(function (string $message) {
+    echo $message . "\n";
+});
+
+$loop->addSingularTimer(10, function () use ($client) {
+    $client->close();
+});
+$loop->run();
