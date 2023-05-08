fd0425c13c269fa71a5fa45fc594e706b9032da6
Pavlenko Eugene <pavlenko.obs@gmail.com>
2023-05-08 12:31:10 +0300

    upd

--- demo/v4-client.php
+++ demo/v4-client.php
@@ -3,8 +3,8 @@
 namespace PE\Component\SMPP\V4;
 
 use PE\Component\Loop\Loop;
-use PE\Component\SMPP\Socket\Factory;
-use PE\Component\SMPP\Socket\Select;
+use PE\Component\Socket\Factory;
+use PE\Component\Socket\Select;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -14,7 +14,7 @@ $loop    = new Loop(1, fn() => $select->dispatch());
 
 $client = $factory->createClient('127.0.0.1:2775');
 $client->setInputHandler(function (string $data) use ($client) {
-    echo "$data\n";
+    echo trim($data) . "\n";
     $client->write("HELLO\n");
 });
 $client->setErrorHandler(function (\Throwable $throwable) {
--- demo/v4-server.php
+++ demo/v4-server.php
@@ -3,9 +3,9 @@
 namespace PE\Component\SMPP\V4;
 
 use PE\Component\Loop\Loop;
-use PE\Component\SMPP\Socket\ClientInterface;
-use PE\Component\SMPP\Socket\Factory;
-use PE\Component\SMPP\Socket\Select;
+use PE\Component\Socket\ClientInterface;
+use PE\Component\Socket\Factory;
+use PE\Component\Socket\Select;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -13,26 +13,29 @@ $select  = new Select();
 $factory = new Factory($select);
 $loop    = new Loop(1, fn() => $select->dispatch());
 
-$client = null;
 $server = $factory->createServer('127.0.0.1:2775');
-$server->setInputHandler(function (ClientInterface $socket) use (&$client) {
+$server->setInputHandler(function (ClientInterface $socket) use ($loop) {
     $client = $socket;
     $client->setInputHandler(function (string $data) {
-        echo "$data\n";
+        echo trim($data) . "\n";
     });
     $client->write("WELCOME\n");
+
     echo "new connection from {$client->getRemoteAddress()}\n";
+
+    $loop->addSingularTimer(10, function () use (&$client) {
+        echo "disconnect client {$client->getRemoteAddress()}\n";
+        $client->close();
+    });
 });
+
 $server->setErrorHandler(function (\Throwable $throwable) {
     echo $throwable . "\n";
 });
+
 $server->setCloseHandler(function (string $message) {
     echo $message . "\n";
 });
 
-$loop->addSingularTimer(10, function () use (&$client) {
-    echo "disconnect client {$client->getRemoteAddress()}\n";
-    $client->close();
-});
 echo "listen to {$server->getAddress()}\n";
 $loop->run();
