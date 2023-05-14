5f5b1b077d9851dac29b12f9e3ae312a52f32946
Pavlenko Eugene <pavlenko.obs@gmail.com>
2023-05-14 11:35:07 +0300

    upd

--- src/ClientAPI.php
+++ src/ClientAPI.php
@@ -3,7 +3,9 @@
 namespace PE\Component\SMPP;
 
 use PE\Component\SMPP\DTO\Deferred;
+use PE\Component\SMPP\DTO\Message;
 use PE\Component\SMPP\DTO\PDU;
+use PE\Component\SMPP\Exception\InvalidArgumentException;
 
 final class ClientAPI
 {
@@ -14,8 +16,23 @@ final class ClientAPI
         $this->client = $client;
     }
 
-    public function submitSM(array $params): Deferred
+    public function submitSM(Message $message, array $params = []): Deferred
     {
+        if (empty($message->getTargetAddress()) || empty($message->getBody())) {
+            throw new InvalidArgumentException('Message body and target address required for SUBMIT_SM');
+        }
+
+        $params = [
+            PDU::KEY_SRC_ADDRESS        => $message->getSourceAddress(),
+            PDU::KEY_DST_ADDRESS        => $message->getTargetAddress(),
+            PDU::KEY_SCHEDULED_AT       => $message->getScheduledAt(),
+            PDU::KEY_VALIDITY_PERIOD    => $message->getExpiredAt(),
+            PDU::KEY_DATA_CODING        => $message->getDataCoding(),
+            PDU::KEY_SM_DEFAULT_MSG_ID  => null,
+            PDU::KEY_SM_LENGTH          => strlen($message->getBody()),
+            PDU::KEY_SHORT_MESSAGE      => $message->getBody(),
+        ] + $params;
+
         return $this->client->send(PDU::ID_SUBMIT_SM, $params);
     }
 
@@ -24,14 +41,29 @@ final class ClientAPI
         return $this->client->send(PDU::ID_DATA_SM, $params);
     }
 
-    public function querySM(array $params): Deferred
+    public function querySM(Message $message): Deferred
     {
-        return $this->client->send(PDU::ID_QUERY_SM, $params);
+        if (empty($message->getID())) {
+            throw new InvalidArgumentException('Message ID required for QUERY_SM');
+        }
+
+        return $this->client->send(PDU::ID_QUERY_SM, [
+            PDU::KEY_MESSAGE_ID  => $message->getID(),
+            PDU::KEY_SRC_ADDRESS => $message->getSourceAddress(),
+        ]);
     }
 
-    public function cancelSM(array $params): Deferred
+    public function cancelSM(Message $message): Deferred
     {
-        return $this->client->send(PDU::ID_CANCEL_SM, $params);
+        if (empty($message->getID()) && empty($message->getSourceAddress())) {
+            throw new InvalidArgumentException('Message ID or source address required for CANCEL_SM');
+        }
+
+        return $this->client->send(PDU::ID_CANCEL_SM, [
+            PDU::KEY_MESSAGE_ID  => $message->getID(),
+            PDU::KEY_SRC_ADDRESS => $message->getSourceAddress(),
+            PDU::KEY_DST_ADDRESS => $message->getTargetAddress(),
+        ]);
     }
 
     public function replaceSM(array $params): Deferred
