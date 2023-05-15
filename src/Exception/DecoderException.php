1d58ffce977f99c3abdb588997d75f0bb70ab350
Pavlenko Eugene <pavlenko.obs@gmail.com>
2023-05-15 08:55:02 +0300

    upd

--- src/Exception/DecoderException.php
+++ src/Exception/DecoderException.php
@@ -4,4 +4,8 @@ namespace PE\Component\SMPP\Exception;
 
 final class DecoderException extends \UnexpectedValueException implements ExceptionInterface
 {
+    public function __construct(string $message, int $status = 0, \Throwable $previous = null)
+    {
+        parent::__construct($message, $status, $previous);
+    }
 }
