6443e5995390159979e258f9fce51d7b05de1535
Pavlenko Eugene <pavlenko.obs@gmail.com>
2023-05-16 15:09:06 +0300

    upd

--- dump.php
+++ dump.php
@@ -2,10 +2,36 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$buffer = '';
+$buffer = "HELLO";
+$pos = 0;
 
-// Check result on empty buffer
-dump('uint08', @unpack('C', $buffer));
-dump('uint16', @unpack('n', $buffer));
-dump('uint32', @unpack('N', $buffer));
-dump(0b00_11_11_00, 0b00_00_00_00, 0b00_00_00_00 & 0b00_11_11_00);
+function decodeString(string $buffer, int &$pos, ?int $min, ?int $max): string
+{
+    $data = '';
+    while (strlen($buffer) > $pos) {
+        $data .= $buffer[$pos++];
+        if ("\n" === $buffer[$pos - 1]) {
+            dump('stop by NUL');
+            break;
+        }
+        // Check max chars for read, usable for TLV with no NUL terminated strings
+        if (strlen($data) === $max) {
+            dump('stop by max');
+            break;
+        }
+    }
+    $pos++;
+    if (null === $max && "\0" !== substr($data, -1)) {
+        dump('not null terminated');//TODO to validator, malformed
+    }
+    if (null !== $min && strlen($data) < $min) {
+        dump('too short');//TODO to validator, invalid???
+    }
+    if ($data === $buffer) {
+        dump('stop by EOF');//just info
+    }
+    return $data;
+}
+
+$str = decodeString($buffer, $pos, null, null);
+dump($str, strlen($str), $pos - 1);
