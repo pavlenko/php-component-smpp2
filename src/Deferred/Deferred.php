a46c591c5b9d0c78c8cea369c381a9372b6515d5
Pavlenko Eugene <pavlenko.obs@gmail.com>
2023-05-18 19:21:00 +0300

    upd

--- src/Deferred/Deferred.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace PE\Component\SMPP\Deferred;
-
-//TODO normalize api
-class Deferred
-{
-
-}
--- src/Deferred/FailureHandlerInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace PE\Component\SMPP\Deferred;
-
-interface FailureHandlerInterface
-{
-
-}
